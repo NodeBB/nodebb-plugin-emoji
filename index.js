3989d5d258bcb31fc4d8d1f53f2eee0dcae59347
Julian Lam <julian.lam@gmail.com>
2013-10-27 16:39:56 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var	emoji = require('emoji-images'),
+	nconf = module.parent.require('nconf'),
+	Emoji = {};
+
+Emoji.addEmoji = function(postContent) {
+	postContent = emoji(postContent, nconf.get('url') + 'plugins/nodebb-plugin-emoji/pngs');
+
+	return postContent;
+};
+
+module.exports = Emoji;
