db43d6a52ee83cd77fd3ae7889c147fc1a3d7afd
frissdiegurke <frissdiegurke@hotmail.com>
2014-02-08 03:35:27 +0100

    added exclusion of code-blocks and mapping of common emotes

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 var	emoji = require('emoji-images'),
 	nconf = module.parent.require('nconf'),
-	Emoji = {};
+	Emoji = {},
+	mapTest = /(^|\s|<(br|p)>)(:[=-]?[\(\)\|\*\$@#\?]|:[=-]?&amp;|;[=-]?[\(\)]|[8b][=-]?[\)\|]|[\|i][-=]?[\(\)]|x[-=]?[\(\)]|:&quot;>|:\^\)|;\/\)|\?{3}|z{3}|o_o)|(^|\s|<(br|p)>)(&lt;3)(<\/?\w+>|\s|$)|(:[=-]?[dopsx])(<\/?\w+>|\s|$)/ig,
+	mapping = [
+		/:[-=]?s/i, ':confounded:',
+		/:[-=]?d/i, ':laughing:',
+		/:[-=]?o/i, ':hushed:',
+		/:[-=]?[x#]/i, ':no_mouth:',
+		/:[-=]?[\*p]/i, ':stuck_out_tongue_winking_eye:',
+		/:[-=]?\)/, ':grinning:',
+		/:[-=]?\(/, ':frowning:',
+		/[8b][-=]?[\|\)]/i, ':sunglasses:',
+		/;[-=]?\)/, ':wink:',
+		/;[-=]?\(/, ':relieved:',
+		/:[-=]?\|/, ':expressionless:',
+		/:[-=]?\$|:&quot>/, ':blush:',
+		/:\^\)/, ':smirk:',
+		/[\|i][-=]?\)/, ':sleeping:',
+		/\|[-=]?\(/, ':pensive:',
+		/:[-=]?&amp;/, ':mask:',
+		/;\/\)/, ':trollface:',
+		/:[-=]?@/, ':rage:',
+		/x[-=]?\(/i, ':rage:',
+		/:[-=]?\?/, ':confused:',
+		/\?\?\?/, ':question:',
+		/&lt;3/, ':heart:',
+		/zzz/i, ':zzz:',
+		/o_o/i, ':eyes:'
+	];
+
+function execMapping(content) {
+	return content.replace(mapTest, function(match) {
+		for (var i = 0; i < mapping.length; i ++) {
+			var m = match.replace(mapping[i], mapping[++i]);
+			if (m != match)
+				return m;
+		}
+		return match;
+	});
+}
 
 Emoji.addEmoji = function(postContent) {
-	postContent = emoji(postContent, nconf.get('url') + '/plugins/emoji-images');
-
-	return postContent;
+	return postContent.replace(/(^|<\/code>)([^<]*|<(?!code>))*(<code>|$)/g, function (match) {
+		return emoji(execMapping(match), nconf.get('url') + '/plugins/emoji-images');
+	});
 };
 
 module.exports = Emoji;
