9924c1f27a3912300a1781355c4117096ff42113
Barış Soner Uşaklı <barisusakli@gmail.com>
2026-06-22 13:04:59 -0400

    fix: use asset_base_url

--- public/lib/types.d.ts
+++ public/lib/types.d.ts
@@ -4,7 +4,7 @@ declare type Callback<T = void> = (result: T) => void;
 
 declare const config: {
   relative_path: string;
-  assetBaseUrl: string;
+  asset_base_url: string;
   'cache-buster': string;
   emojiCustomFirst: boolean;
   csrf_token: string;
